95e889c92dc1368207f95b395a36a465022b16f6
kreevkeet <78335901+kreevkeet@users.noreply.github.com>
2021-02-01 11:51:58 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ app.set('views', './views');
 const port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-    res.render('index', {title: 'Заголовок', message: 'Тут текст!'});
+    res.render('index', {title: 'Заголовок', message: 'УТКОНОСЫ ЗАХВАТЯТ МИР'});
 });
 
-app.get('/page3', function (req, res) {
-    res.render('page2', {title: 'Страница 2', message: 'Текст второй страницы'});
+app.get('/page2', function (req, res) {
+    res.render('page2', {title: 'Страница 2', message: 'План по захвату мира утконосами:'});
 });
 
 app.listen(port, function() {
